b701563823fd03209b6c87acb421a2469b89a0f9
Caio Fabio <109986771+caiofaabio@users.noreply.github.com>
2024-01-02 20:24:35 -0300

    feat: snacks

--- src/pages/Main/Burguers/index.tsx
+++ src/pages/Main/Burguers/index.tsx
@@ -1,5 +1,6 @@
 import Head from '../../../components/Head'
 import SnackTitle from '../../../components/SnackTitle'
+import Snacks from '../Snacks'
 
 export default function Burguers (){
   const data = [
@@ -17,7 +18,7 @@ export default function Burguers (){
       name: 'Extra bacon',
       description: 'O artesanal recheado de bacon crocante, queijo e carne.',
       price: 23.5,
-      image: 'https://instadelivery-public.nyc3.cdn.digitaloceanspaces.com/itens/5BvH5nLn4cTnYNqbjMXCKAQlaGgrBNCSpQBAPi3N.png'
+      image: 'https://www.estadao.com.br/resizer/YIBfPUoXUOKRYG7IVo-jptwsSFY=/720x503/filters:format(jpg):quality(80):focal(3070x2285:3080x2295)/cloudfront-us-east-1.images.arcpublishing.com/estadao/L3LYN5Y4MRG6BB47MNHEEXDRGA.jpeg'
     }
   ]
 
@@ -26,6 +27,8 @@ export default function Burguers (){
     <>
       <Head title='Hambúrguers' description='Sua página de Hamburguers' />
       <SnackTitle>Hambúrguers</SnackTitle>
+
+      <Snacks snacks={data} />
     </>
   )
 }
---
+++ src/pages/Main/Snacks/style.ts
@@ -0,0 +1,14 @@
+import styled from "styled-components";
+
+export const Container = styled.div`
+  display: grid;
+  grid-template-columns: repeat(auto-fit, minmax(300px, auto));
+  gap: 1.75rem;
+
+
+  @media (max-width: 500px){
+    grid-template-columns: 1fr;
+  }
+
+  border: 1px solid red;
+`
